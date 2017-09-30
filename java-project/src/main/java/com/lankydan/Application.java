c3b93b7b1aabc2b793a19091a25653a3342866a5
Dan Newton <danknewton@hotmail.com>
2017-09-30 10:27:26 +0100

    commit

--- java-project/src/main/java/com/lankydan/Application.java
+++ java-project/src/main/java/com/lankydan/Application.java
@@ -1,15 +1,12 @@
-package com.yourself;
+package com.lankydan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by charlotte on 06/06/17.
- */
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String args[]) {
+    SpringApplication.run(Application.class);
+  }
 }
